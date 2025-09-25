e08e9e6c29cb03ae5020f3d1b7b0248b5f56b69b
이상윤 <sylagape1231@gmail.com>
2025-09-25 10:35:36 +0900

    Add: 파일 추가

--- learn-react/06/1/src/App.tsx
+++ learn-react/06/1/src/App.tsx
@@ -1,21 +1,52 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Input from "./components/Input";
 
-function App() {
-  const [user, setUser] = useState({ name: "mike", age: 30 });
-  const handleUpdate = () => {
-    // setUser({
-    //   name: "Lee",
-    //   age: 30
-    // })
-    setUser((user) => ({ ...user, age: 40 }));
+export default function App() {
+  const [email, setEmail] = useState("");
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const [pw, setPw] = useState("");
+  const pwRef = useRef<HTMLInputElement>(null);
+
+  const handleEmailUpate = (value: string) => {
+    setEmail(value);
+  };
+  const handlePwUpate = (value: string) => {
+    setPw(value);
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (email.trim() === "") {
+      alert("이메일을 입력해주세요");
+      if (emailRef.current) emailRef.current.focus();
+      return;
+    }
+    if (pw.trim() === "") {
+      alert("비밀번호를 입력해주세요");
+      if (pwRef.current) pwRef.current.focus();
+      return;
+    }
+    console.log(email, pw);
   };
   return (
-    <div>
-      <h1>Name: {user.name}</h1>
-      <h1>Age: {user.age}</h1>
-      <button onClick={handleUpdate}>Update</button>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          ref={emailRef}
+          placeholder="이메일 입력"
+          value={email}
+          setValue={handleEmailUpate}
+        />
+        <Input
+          type="password"
+          ref={pwRef}
+          placeholder="비밀번호 입력"
+          value={pw}
+          setValue={handlePwUpate}
+        />
+        <button>로그인</button>
+      </form>
+    </>
   );
 }
-
-export default App;
